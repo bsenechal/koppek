896fcf81036a340d4fb781096a6a9050214c34db
bsenechal <benoit.senechal1993@gmail.com>
2015-09-25 16:37:40 +0200

    Correction du menu

--- public/modules/admin/config/admin.client.config.js
+++ public/modules/admin/config/admin.client.config.js
@@ -4,8 +4,8 @@
 angular.module('admin').run(['Menus',
    function(Menus) {
       // Set top bar menu items
-      Menus.addMenuItem('topbar', 'Admin', 'admin', 'dropdown', '');
-      Menus.addSubMenuItem('topbar', 'admin', 'Admin Deal', 'admin/deals');
+      Menus.addMenuItem('topbar', 'Admin', 'admin', 'dropdown', '', false, ['admin']);
+      Menus.addSubMenuItem('topbar', 'admin', 'Admin Deal', 'admin/deals', false,  'admin/deals', ['admin']);
       // Menus.addSubMenuItem('topbar', 'articles', 'New Article', 'articles/create');
    }
 ]);
